4b826e5dedd2724d8d864321cd81dd82a747dd07
Robert Voyer <robert.voyer@socrata.com>
2016-12-01 15:18:32 -0800

    Seems to be working...

--- pyreleaseplugin/release.py
+++ pyreleaseplugin/release.py
@@ -21,7 +21,7 @@ The release command looks for a setup.cfg file in the current directory. If any 
 not passed in on the command-line, it will look for them in the setup.cfg configuration file (under
 a section named "release").
 
-Testing again and again...
+Seems to be working...
 """
 
 from datetime import datetime
