5b6ff6d0f1054a5e5bb292aa46b6464fd7a00451
Josh Oberwetter <josh.oberwetter@socrata.com>
2017-04-27 11:46:24 -0700

    EN-15980: fix prospector warnings
    Also, add dev guide to the README. Matches the details in
    https://github.com/socrata/python-clients-common.

--- pyreleaseplugin/__init__.py
+++ pyreleaseplugin/__init__.py
@@ -1,3 +1,6 @@
+# this is a common convenience pattern in python, but it confused pyflakes
+# (F401 imported but unused), so let's tell pyflakes to ignore this file.
+# flake8: noqa
 from pyreleaseplugin.clean import CleanCommand
 from pyreleaseplugin.release import ReleaseCommand
 from pyreleaseplugin.test import PyTest
