62a87f60ea1fa8cb3f01f406ed511ea90f660e18
jvergara <Julio.Vergara-Heinrroth@medecision.com>
2021-12-21 19:39:47 -0500

    remove accidentally committed files

--- build-install-local-pyreleaseplugin.sh
+++
@@ -1,8 +0,0 @@
-# Build Python Release Plugin
-python3 setup.py clean build bdist_wheel
-
-#Install Python Release Plugin locally
-pip3 install dist/pyreleaseplugin-0.2.15-py3-none-any.whl 
-
-# Show Python Release Plugin installed using pip3, to confirm installation
-pip3 show pyreleaseplugin 
--- uninstall-pyreleaseplugin.sh
+++
@@ -1,6 +0,0 @@
-# Show Python Release Plugin installed using pip3
-pip3 show pyreleaseplugin 
-# Uninstall Python Release Plugin
-pip3 uninstall pyreleaseplugin 
-# Show Python Release Plugin installed using pip3, to confirm uninstallation
-pip3 show pyreleaseplugin 
